1f69d787f90efc501a59836263fe0a0a226a9cb0
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2017-04-01 12:25:34 +0200

    [Rust] Rust by Example - Scoping - Lifetimes

--- Rust/Language/rust_by_example/src/scoping.rs
+++ Rust/Language/rust_by_example/src/scoping.rs
@@ -4,6 +4,7 @@ pub fn scoping() {
     raii();
     ownership_and_moves();
     borrowing();
+    lifetimes();
 
     println!("");
 }
@@ -259,5 +260,243 @@ fn borrowing() {
         }
 
         println!("tuple is: {:?}", mutable_tuple);
+    }
+}
+
+fn lifetimes() {
+    /* explicit annotation */
+    {
+        // fn takes two references to i32 which have different lifetimes
+        // 'a and 'b. These two lifetimes must both be at least as long
+        // as the function print_refs
+        fn print_refs<'a, 'b>(x: &'a i32, y: &'b i32) {
+            println!("x is {} and y is {}", x, y);
+        }
+
+        // a function which takes no arguments
+        // but has a lifetime parameter a
+        fn failed_borrow<'a>() {
+            let _x = 12;
+
+            // _x does not live long enough
+            // let y: &'a i32 = &_x;
+            // attempting to use the lifetime 'a as an explicit type annotation
+            // inside the function will fail because the lifetime &_x is shorter
+            // than that of y
+        }
+
+        let (four, nine) = (4, 9);
+        // passes borrows into a function
+        print_refs(&four, &nine);
+        // any input which is borrowed must outlive the borrower
+        // the lifetime of four and nine must be longer
+        // than that of print_refs
+
+        failed_borrow();
+        // it contains no references to force 'a to be
+        // longer than the lifetime of the function but 'a is longer
+        // because the lifetime is not constrained, it defaults to static
+    }
+
+    /* functions */
+    {
+        // input with lifetime 'a must live at least as long as the function
+        fn print_one<'a>(x: &'a i32) {
+            println!("print_one: x is {}", x);
+        }
+
+        // mutable references are possible with lifetimes as well
+        fn add_one<'a>(x: &'a mut i32) {
+            *x += 1;
+        }
+
+        // multiple elements with different lifetimes
+        // in this case it would be fine for both to have the same lifetime 'a
+        fn print_multi<'a, 'b>(x: &'a i32, y: &'b i32) {
+            println!("print_multi: x is {}, y is {}", x, y);
+        }
+
+        // returning references that have been passed in is acceptable
+        // the correct lifetime must be returned
+        fn pass_x<'a, 'b>(x: &'a i32, _: &'b i32) -> &'a i32 {
+            x
+        }
+
+        let (x, y) = (7, 9);
+        print_one(&x);
+        print_multi(&x, &y);
+
+        let z = pass_x(&x, &y);
+        print_one(z);
+
+        let mut t = 3;
+        add_one(&mut t);
+        print_one(&t);
+    }
+
+    /* methods */
+    {
+        struct Owner(i32);
+
+        impl Owner {
+            fn add_one<'a>(&'a mut self) {
+                self.0 += 1
+            }
+
+            fn print<'a>(&'a self) {
+                println!("print: {}", self.0);
+            }
+        }
+
+        let mut owner = Owner(18);
+        owner.add_one();
+        owner.print();
+    }
+
+    /* structs */
+    {
+        // a reference to i32 must outlive the struct
+        #[derive(Debug)]
+        struct Borrowed<'a>(&'a i32);
+
+        // both references must outlive the structure
+        #[derive(Debug)]
+        struct NamedBorrowed<'a> {
+            x: &'a i32,
+            y: &'a i32,
+        }
+
+        // an enum which is either an i32 or a reference to one
+        #[derive(Debug)]
+        enum Either<'a> {
+            Num(i32),
+            Ref(&'a i32),
+        }
+
+        let x = 18;
+        let y = 15;
+
+        let single = Borrowed(&x);
+        let double = NamedBorrowed {
+            x: &x,
+            y: &y,
+        };
+        let reference = Either::Ref(&x);
+        let number = Either::Num(y);
+
+        println!("x is borrowed in {:?}", single);
+        println!("x and y are borrowed in {:?}", double);
+        println!("x is borrowed in {:?}", reference);
+        println!("y is *not* borrowed in {:?}", number);
+    }
+
+    /* bounds */
+    {
+        use std::fmt::Debug;
+
+        #[derive(Debug)]
+        struct Ref<'a, T: 'a>(&'a T);
+        // Ref contains a reference to generic type T that has an unknown lifetime 'a
+        // T is bounded such that any references in T must outlive 'a
+        // the lifetime of Ref may not exceed 'a
+
+        fn print<T>(t: T) where T: Debug {
+            println!("print: t is {:?}", t);
+        }
+
+        // a reference to T is taken where T implements Debug
+        // all references in T outlive 'a
+        // 'a must outlive the function
+        fn print_ref<'a, T>(t: &'a T) where T: Debug + 'a {
+            println!("print_ref: t is {:?}", t);
+        }
+
+        let x = 7;
+        let ref_x = Ref(&x);
+
+        print_ref(&ref_x);
+        print(ref_x);
+    }
+
+    /* coercion */
+    {
+        // Rust infers a lifetime that is as short as possible
+        // two references are then coerced to that lifetime
+        fn multiply<'a>(first: &'a i32, second: &'a i32) -> i32 {
+            first * second
+        }
+
+        // <'a: 'b, 'b> - lifetime 'a is at least as long as 'b
+        // we take in an &'a i32 and return a &'b i32 as a result of coercion
+        fn choose_first<'a: 'b, 'b>(first: &'a i32, _: &'b i32) -> &'b i32 {
+            first
+        }
+
+        let first = 2; // longer lifetime
+
+        {
+            let second = 3; // shorter lifetime
+            println!("the product is {}", multiply(&first, &second));
+
+            println!("{} is the first", choose_first(&first, &second));
+        }
+    }
+
+    /* static */
+    {
+        // a constant with a 'static lifetime
+        static NUM: i32 = 18;
+
+        // returns a reference to NUM where its static
+        // lifetime is coerced to that of the input argument
+        fn coerce_static<'a>(_: &'a i32) -> &'a i32 {
+            &NUM
+        }
+
+        {
+            let static_string = "I'm in read-only memory";
+            println!("static_string: {}", static_string);
+
+            // when the reference goes out of scope, str can no longer be used
+            // but the data remains in the binary
+        }
+
+        {
+            let lifetime_num = 9;
+
+            // coerce NUM to the lifetime of lifetime_num
+            let coerced_static = coerce_static(&lifetime_num);
+
+            println!("coerced static: {}", coerced_static);
+        }
+
+        println!("NUM: {} stays accessible!", NUM);
+    }
+
+    /* elision */
+    {
+        fn elided_input(x: &i32) {
+            println!("elided_input: {}", x);
+        }
+
+        fn annotated_input<'a>(x: &'a i32) {
+            println!("annotated_input: {}", x);
+        }
+
+        fn elided_pass(x: &i32) -> &i32 {
+            x
+        }
+
+        fn annotated_pass<'a>(x: &'a i32) -> &'a i32 {
+            x
+        }
+
+        let x = 3;
+
+        elided_input(&x);
+        annotated_input(&x);
+
+        println!("elided_pass: {}", elided_pass(&x));
+        println!("annotated pass: {}", annotated_pass(&x));
     }
 }
