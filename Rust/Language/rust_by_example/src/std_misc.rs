f2a64a982731e5ab9caf3a412f95f459fe1d398f
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2017-04-06 20:28:59 +0200

    [Rust] Rust by Example - STD Misc 2/2
    Child processes
    Filesystem operations
    Program arguments

--- Rust/Language/rust_by_example/src/std_misc.rs
+++ Rust/Language/rust_by_example/src/std_misc.rs
@@ -5,6 +5,9 @@ pub fn std_misc() {
     channels();
     paths();
     // file_io();
+    // child_processes();
+    // filesystem_operations();
+    program_arguments();
 
     println!("");
 }
@@ -148,6 +151,248 @@ fn file_io() {
             Err(why) => panic!("couldn't write to {}: {}", display,
                                why.description()),
             Ok(_) => println!("successfully wrote to {}", display),
+        }
+    }
+}
+
+fn child_processes() {
+    {
+        use std::process::Command;
+
+        let output = Command::new("rustc")
+            .arg("--version")
+            .output().unwrap_or_else(|e| {
+                panic!("Failed to execute a process: {}", e);
+            });
+
+        if output.status.success() {
+            let s = String::from_utf8_lossy(&output.stdout);
+
+            print!("rustc succeded and stdout was:\n{}", s);
+        } else {
+            let s = String::from_utf8_lossy(&output.stderr);
+
+            print!("rustc failed and stderr was:\n{}", s);
+        }
+    }
+
+    /* pipes */
+    {
+        use std::error::Error;
+        use std::io::prelude::*;
+        use std::process::{Command, Stdio};
+
+        static PANGRAM: &'static str =
+            "the quick brown fox jumped over the lazy dog\n";
+
+        // spawn the wc command
+        let process = match Command::new("wc")
+            .stdin(Stdio::piped())
+            .stdout(Stdio::piped())
+            .spawn() {
+                Err(why) => panic!("Couldn't spawn wc: {}", why.description()),
+                Ok(process) => process,
+            };
+
+        // write a string to the stdin of wc
+        // stdin is Option<ChildStdin>, we can directly unwrap it here
+        match process.stdin.unwrap().write_all(PANGRAM.as_bytes()) {
+            Err(why) => panic!("couldn't write to wc stdin: {}",
+                               why.description()),
+            Ok(_) => println!("sent pangram to wc"),
+        }
+
+        // stdin does not live after the above calls
+        // it's dropped and the pipe is closed
+        // otherwise wc wouldn't start processing the input we sent
+
+        // stdout is Option<ChildStdout>
+        let mut s = String::new();
+        match process.stdout.unwrap().read_to_string(&mut s) {
+            Err(why) => panic!("couldn't read wc stdout: {}",
+                               why.description()),
+            Ok(_) => print!("wc responded with:\n{}", s),
+        }
+
+    }
+
+    /* wait */
+    {
+        use std::process::Command;
+
+        let mut child = Command::new("sleep").arg("1").spawn().unwrap();
+        let _result = child.wait().unwrap();
+
+        println!("reached the end!");
+    }
+}
+
+fn filesystem_operations() {
+    use std::fs;
+    use std::fs::{File, OpenOptions};
+    use std::io;
+    use std::io::prelude::*;
+    use std::os::unix;
+    use std::path::Path;
+
+    // a simple implementation of 'cat path'
+    fn cat(path: &Path) -> io::Result<String> {
+        let mut f = try!(File::open(path));
+        let mut s = String::new();
+        match f.read_to_string(&mut s) {
+            Ok(_) => Ok(s),
+            Err(e) => Err(e),
+        }
+    }
+
+    // a simple implementation of 'echo s > path'
+    fn echo(s: &str, path: &Path) -> io::Result<()> {
+        let mut f = try!(File::create(path));
+
+        f.write_all(s.as_bytes())
+    }
+
+    // a simple implementation of 'touch path' (ignores existing files)
+    fn touch(path: &Path) -> io::Result<()> {
+        match OpenOptions::new().create(true).write(true).open(path) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(e),
+        }
+    }
+
+    println!("mkdir a");
+    // create a directory, returns io::Result<()>
+    match fs::create_dir("a") {
+        Err(e) => println!("! {:?}", e.kind()),
+        Ok(_) => {},
+    }
+
+    println!("'echo hello > a/b.txt'");
+    // the previous match can be simplified with unwrap_or_else
+    echo("hello", &Path::new("a/b.txt")).unwrap_or_else(|e| {
+        println!("! {:?}", e.kind());
+    });
+
+    println!("mkdir -p a/c/d '");
+    // recursively create a directory
+    fs::create_dir_all("a/c/d").unwrap_or_else(|e| {
+        println!("! {:?}", e.kind());
+    });
+
+    println!("touch a/c/e.txt");
+    touch(&Path::new("a/c/e.txt")).unwrap_or_else(|e| {
+        println!("! {:?}", e.kind());
+    });
+
+    println!("ln -s ../b.txt a/c/b.txt");
+    // create a symlink
+    if cfg!(target_family = "unix") {
+        unix::fs::symlink("../b.txt", "a/c/b.txt").unwrap_or_else(|e| {
+            println!("! {:?}", e.kind());
+        });
+    }
+
+    println!("cat a/c/b.txt");
+    match cat(&Path::new("a/c/b.txt")) {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(s) => println!("> {}", s),
+    }
+
+    println!("ls a");
+    // read the contents of a directory, returns io::Result<Vec<Path>>
+    match fs::read_dir("a") {
+        Err(why) => println!("! {:?}", why.kind()),
+        Ok(paths) => for path in paths {
+            println!("> {:?}", path.unwrap().path());
+        },
+    }
+
+    println!("rm a/c/e.txt");
+    fs::remove_file("a/c/e.txt").unwrap_or_else(|e| {
+        println!("! {:?}", e.kind());
+    });
+
+    println!("rmdir a/c/d");
+    // remove an empty directory
+    fs::remove_dir("a/c/d").unwrap_or_else(|e| {
+        println!("! {:?}", e.kind());
+    });
+}
+
+fn program_arguments() {
+    /* basics */
+    {
+        use std::env;
+
+        let args: Vec<String> = env::args().collect();
+
+        // the first argument is the path
+        println!("My path is {}", args[0]);
+
+        // the rest of the arguments are passed command line parameters
+        println!("I got {:?} arguments: {:?}", args.len()-1, &args[1..]);
+    }
+
+    /* argument parsing */
+    {
+        use std::env;
+
+        fn increase(number: i32) {
+            println!("{}", number+1);
+        }
+
+        fn decrease(number: i32) {
+            println!("{}", number-1);
+        }
+
+        fn help() {
+            println!("usage:
+match_args <string>
+     Check whether given string is the answer.
+match_args {{increase|decrease}} <integer>
+     Increase or decrease given integer by one");
+        }
+
+        let args: Vec<String> = env::args().collect();
+
+        match args.len() {
+            // no arguments
+            1 => println!("my name is match_args, pass some arguments!"),
+            // one argument
+            2 => {
+                match args[1].parse() {
+                    Ok(42) => println!("This is the answer!"),
+                    _ => println!("This is not the answer!"),
+                }
+            },
+            // one command and one argument passed
+            3 => {
+                let cmd = &args[1];
+                let num = &args[2];
+                // parse the number
+                let number: i32 = match num.parse() {
+                    Ok(n) => n,
+                    Err(_) => {
+                        println!("error: second argument is not an integer!");
+                        help();
+                        return;
+                    },
+                };
+                // parse the command
+                match &cmd[..] {
+                    "increase" => increase(number),
+                    "decrease" => decrease(number),
+                    _ => {
+                        println!("error: invalid command");
+                        help();
+                    },
+                }
+            },
+            // all other cases
+            _ => {
+                // show a help message
+                help();
+            }
         }
     }
 }
