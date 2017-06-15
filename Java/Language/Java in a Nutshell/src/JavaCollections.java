24147403f8578b43adbec8cf19ec2bfaad56a7e9
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2017-06-15 21:18:09 +0200

    [Java] Java in a Nutshell - Working with Java Collections #2
    * Queue and Blocking Queue
    * Wrappers
    * Utility Methods
    * Arrays and Helper Methods

--- Java/Language/Java in a Nutshell/src/JavaCollections.java
+++ Java/Language/Java in a Nutshell/src/JavaCollections.java
@@ -1,6 +1,9 @@
 import com.sun.org.apache.xpath.internal.SourceTree;
 
+import java.awt.*;
+import java.lang.reflect.Array;
 import java.util.*;
+import java.util.List;
 
 /**
  * Created by cajetan on 6/7/17.
@@ -83,5 +86,69 @@ public class JavaCollections {
         assert mapOne.size() == 0;
         assert mapOne.isEmpty();
         assert mapOne.equals(empty);
+    }
+
+    public static void wrappers() {
+        List<String> sync_list = Collections.synchronizedList(new ArrayList<String>());
+        Set<Integer> set = Collections.synchronizedSet(new HashSet<Integer>());
+        Map<String,Integer> map = Collections.synchronizedMap(new HashMap<String,Integer>());
+
+        List<Integer> primes = new ArrayList<Integer>();
+        List<Integer> readonly = Collections.unmodifiableList(primes);
+        primes.addAll(Arrays.asList(2, 3, 5, 7, 11, 13, 17, 19));
+        // readonly.add(23); // unsupported operation exception
+
+
+    }
+
+    public static void utilities() {
+        List<Integer> list1 = Arrays.asList(1, 8, 4, 3, 9, 0);
+        List<Integer> list2 = Arrays.asList(1, 9);
+
+        Collections.sort(list1);
+        int pos = Collections.binarySearch(list1, 3);
+
+        Collections.copy(list1, list2);
+        Collections.fill(list2, 8);
+        Collections.max(list1);
+        Collections.min(list1);
+
+        Collections.reverse(list1);
+        Collections.shuffle(list2);
+
+        Set<Integer> s1 = Collections.emptySet();
+        List<String> s2 = Collections.emptyList();
+        Map<String, Integer> m = Collections.emptyMap();
+
+        List<Integer> tenzeros = Collections.nCopies(10, 0);
+    }
+
+    public static void arraysAndHelpers() {
+        String[] a = { "this", "is", "a", "test" };
+        List<String> l = Arrays.asList(a);
+        List<String> m = new ArrayList<>(l);
+
+        String[] c = l.toArray(new String[0]);
+
+        char[] text = "Now is the time".toCharArray();
+        char[] copy = new char[100];
+        System.arraycopy(text, 4, copy, 0, 10);
+        System.arraycopy(copy, 3, copy, 6, 7);
+
+        int[] intArray = new int[] { 10, 5, 7, -3 };
+        Arrays.sort(intArray);
+        int pos = Arrays.binarySearch(intArray, 7);
+
+        int[] clone = intArray.clone();
+        boolean b1 = Arrays.equals(intArray, clone);
+
+        byte[] data = new byte[100];
+        Arrays.fill(data, (byte) -1);
+        Arrays.fill(data, 5, 10, (byte) -2);
+
+        Class type = data.getClass();
+        if (type.isArray()) {
+            Class elementType = type.getComponentType();
+        }
     }
 }
