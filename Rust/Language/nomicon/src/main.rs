0cd3d71e2db3be07db20f6072e0f8bbcdc8bb8fe
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2017-08-21 20:41:30 +0200

    [Rust] Rustonomicon - Type Conversions

--- Rust/Language/nomicon/src/main.rs
+++ Rust/Language/nomicon/src/main.rs
@@ -1,3 +1,22 @@
+#![allow(dead_code)]
+
 fn main() {
-    println!("Hello, world!");
+    type_conversions();
+}
+
+fn type_conversions() {
+    struct Foo {
+        x: u32,
+        y: u16,
+    }
+
+    struct Bar {
+        a: u32,
+        b: u16,
+    }
+
+    fn reinterpret(foo: Foo) -> Bar {
+        let Foo { x, y } = foo;
+        Bar { a: x, b: y }
+    }
 }
