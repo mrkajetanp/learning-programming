b6e329a993232f84a86646d213001225f7b7bfcf
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2018-04-21 14:47:37 +0200

    [Python] Singleton class example

--- Python/Learning/Language/oop.py
+++ Python/Learning/Language/oop.py
@@ -14,7 +14,33 @@ class C2(object):
         return 2*self.x
 
 
+class C3(object):
+    def hello_there(self):
+        print("General Kenobi!")
+
+
+class Singleton(object):
+    _singletons = {}
+
+    def __new__(cls, *args, **kwds):
+        if cls not in cls._singletons:
+            cls._singletons[cls] = super(Singleton, cls).__new__(cls)
+        return cls._singletons[cls]
+
+    def __init__(self, n):
+        self.x = n
+
+    def double_value(self):
+        return 3*self.x
+
+
 print(C1.x)
 C1.method_one()
 inst = C2(21)
 print(inst.double_value())
+C3().hello_there()
+
+x = Singleton(8)
+print(x.double_value())
+y = Singleton(9)
+print(x.double_value(), y.double_value())
