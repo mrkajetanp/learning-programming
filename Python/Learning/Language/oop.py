a3568a1ed0e3a3bbbd2eb0beb786ea9c3f9eef86
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2018-04-21 14:26:46 +0200

    [Python] Class init example

--- Python/Learning/Language/oop.py
+++ Python/Learning/Language/oop.py
@@ -6,5 +6,15 @@ class C1(object):
         print("C1: {}".format(C1.x*2))
 
 
+class C2(object):
+    def __init__(self, n):
+        self.x = n
+
+    def double_value(self):
+        return 2*self.x
+
+
 print(C1.x)
 C1.method_one()
+inst = C2(21)
+print(inst.double_value())
