0a1fb9a25b1c6dd589eed6ba2d92137f72a6765f
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2018-04-24 09:43:31 +0200

    [Python] Filter and reduce example

--- Python/Learning/Tips/map_filter_reduce.py
+++ Python/Learning/Tips/map_filter_reduce.py
@@ -1,4 +1,13 @@
+from functools import reduce
 
 items = [1, 2, 3, 4, 5]
 squared = list(map(lambda x: x**2, items))
-print(squared)
+print("squared:", squared)
+
+items = [1, -2, 3, 4, -5]
+negative = list(filter(lambda x: x < 0, items))
+print("negative:", negative)
+
+items = [1, 2, 3, 4]
+product = reduce(lambda x, y: x*y, items)
+print("product:", product)
