d486c6f31da5912230d042d3c671b95227d7e1b8
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2018-04-25 03:05:41 +0200

    [Python] defaultdict example

---
+++ Python/Learning/Tips/collect.py
@@ -0,0 +1,11 @@
+from collections import defaultdict
+
+favourites = defaultdict(list)
+favourites['hello'].append("there")
+favourites['hello'].append("yourself")
+
+print(favourites)
+
+dic = {'test': 'test'}
+# dic['hello'].append("there")
+print(dic)
