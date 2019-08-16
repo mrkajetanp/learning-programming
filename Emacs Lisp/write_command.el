4681976af65af4988e641a678c00923a46f2bbbe
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-08-16 12:59:16 +0100

    [Emacs Lisp] Writing a Command - Mark and Region

--- Emacs Lisp/write_command.el
+++ Emacs Lisp/write_command.el
@@ -16,4 +16,59 @@ and don't indent them."
   )
 )
 
+;; Mark and Region
+
+(defun ff-region()
+  (interactive)
+  (message "begin at %s and end at %s" (region-beginning) (region-end))
+)
+
+(kill-region)
+(comment-region)
+(indent-region)
+(fill-region)
+
+(setq mark-active t)
+
+(defun my-is-region-active()
+  "print whether region is active"
+  (interactive)
+  (if (use-region-p)
+      (message "region active")
+    (message "region not active")
+  )
+)
+
+(defun my-select-line()
+  "Select current line"
+  (interactive)
+  (let (p1 p2)
+    (setq p1 (line-beginning-position))
+    (setq p2 (line-end-position))
+    (goto-char p1)
+    (push-mark p2)
+    (setq mark-active t)
+  )
+)
+
+(defun ff-r (p1 p2)
+  "print region begin/end"
+  (interactive "r")
+  (message "Region starts %d, ends %d" p1 p2)
+)
+
+(defun downcase-word-or-region()
+  "Downcase current word or region"
+  (interactive)
+  (let (pos1 pos2 bds)
+    (if (use-region-p)
+        (setq pos1 (region-beginning) pos2 (region-end))
+      (progn
+        (setq bds (bounds-of-thing-at-point 'symbol))
+        (setq pos1 (car bds) pos2 (cdr bds))
+      )
+    )
+    (downcase-region pos1 pos2)
+  )
+)
 
