49db85438ba0ebe780d08eb2459dfd809b52810b
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-09-03 13:32:02 +0100

    [Emacs Lisp] Command Examples - Toggle Letter Case

--- Emacs Lisp/command_examples.el
+++ Emacs Lisp/command_examples.el
@@ -9,3 +9,50 @@
     (setq cmdStr scriptPath)
     (shell-command-on-region startPos endPos cmdStr nil t nil t)))
 
+
+;; Toggle Letter Case
+
+(defun xah-toggle-letter-case ()
+  "Toggle the letter case of current word or text selection.
+Always cycle in this order: Init Caps, ALL CAPS, all lower.
+
+URL `http://ergoemacs.org/emacs/modernization_upcase-word.html'
+Version 2017-04-19"
+  (interactive)
+  (let (
+        (deactivate-mark nil)
+        $p1 $p2)
+    (if (use-region-p)
+        (setq $p1 (region-beginning)
+              $p2 (region-end))
+      (save-excursion
+        (skip-chars-backward "[:alnum:]-_")
+        (setq $p1 (point))
+        (skip-chars-forward "[:alnum:]-_")
+        (setq $p2 (point))))
+    (when (not (eq last-command this-command))
+      (put this-command 'state 0))
+    (cond
+     ((equal 0 (get this-command 'state))
+      (upcase-initials-region $p1 $p2)
+      (put this-command 'state 1))
+     ((equal 1  (get this-command 'state))
+      (upcase-region $p1 $p2)
+      (put this-command 'state 2))
+     ((equal 2 (get this-command 'state))
+      (downcase-region $p1 $p2)
+      (put this-command 'state 0)))))
+
+(defun xah-toggle-previous-letter-case ()
+  "Toggle the letter case of the letter to the left of cursor.
+URL `http://ergoemacs.org/emacs/modernization_upcase-word.html'
+Version 2015-12-22"
+  (interactive)
+  (let ((case-fold-search nil))
+    (left-char 1)
+    (cond
+     ((looking-at "[[:lower:]]") (upcase-region (point) (1+ (point))))
+     ((looking-at "[[:upper:]]") (downcase-region (point) (1+ (point)))))
+    (right-char)))
+
+testing okay okay testing
