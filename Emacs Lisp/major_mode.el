9f69e526d74e0dbcb22ec1244421586f2289ef0b
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-09-18 18:22:24 +0100

    [Emacs Lisp] Major Mode - Lookup Command

--- Emacs Lisp/major_mode.el
+++ Emacs Lisp/major_mode.el
@@ -327,3 +327,23 @@ Version 2016-11-01"
 Version 2016-11-01"
   (interactive "r")
   (remove-overlays @begin @end))
+
+;; Lookup Command
+
+(require 'browse-url) ; part of gnu emacs
+
+(defun my-lookup-wikipedia ()
+  "Look up the word under cursor in Wikipedia.
+If there is a text selection (a phrase), use that.
+
+This command switches to browser."
+  (interactive)
+  (let (word)
+    (setq word
+          (if (use-region-p)
+              (buffer-substring-no-properties (region-beginning) (region-end))
+            (current-word)))
+    (setq word (replace-regexp-in-string " " "_" word))
+    (browse-url (concat "http://en.wikipedia.org/wiki/" word))
+    ;; (eww myUrl) ; emacs's own browser
+    ))
