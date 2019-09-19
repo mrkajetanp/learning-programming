feaab54f57cd8e80de0cb0bbb7824bb4c605c779
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-09-19 17:42:55 +0100

    [Emacs Lisp] Scripts - String inside HTML Tag

--- Emacs Lisp/scripts.el
+++ Emacs Lisp/scripts.el
@@ -31,3 +31,47 @@
     (mapc 'my-process-file
           (directory-files-recursively inputDir "\.html$" ))
     (princ "Done")))
+
+(setq inputDir "/Users/xah/web/wordyenglish_com/arabian_nights/" ) ; dir should end with a slash
+
+;; need sgml-skip-tag-forward
+(require 'sgml-mode)
+
+(defun my-process-file (fPath)
+  "Process the file at FPATH …"
+  (let (myBuffer
+        p3 p4  (bulletCount 0) )
+
+    ;; (print fPath)
+
+    (when
+        (and (not (string-match "/xx" fPath))) ; skip some dir
+
+      (setq myBuffer (get-buffer-create " myTemp"))
+      (set-buffer myBuffer)
+      (insert-file-contents fPath nil nil nil t)
+
+      (setq bulletCount 0 )
+      (goto-char 1)
+      (while
+          (search-forward "<div class=\"xnote\">"  nil t)
+
+        (setq p3 (point)) ; beginning of innerText, after <div class="xnote">
+        (backward-char)
+        (sgml-skip-tag-forward 1)
+        (backward-char 6)
+        (setq p4 (point)) ; end of innerText, before </div>
+
+        (setq bulletCount (count-matches "•" p3 p4))
+
+        (when (> bulletCount 1)
+          (princ (format "Found: %d %s\n" bulletCount fPath))))
+
+      (kill-buffer myBuffer))))
+
+(let (outputBuffer)
+  (setq outputBuffer "*my output*" )
+  (with-output-to-temp-buffer outputBuffer
+    (mapc 'my-process-file
+          (directory-files-recursively inputDir "\.html$" ))
+    (princ "Done")))
