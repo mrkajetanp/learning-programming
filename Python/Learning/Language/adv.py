f1148dca4c14309b371b876c8b6748ff30568786
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-09-21 14:00:48 +0100

    [Python] Advanced - Set Operations

--- Python/Learning/Language/adv.py
+++ Python/Learning/Language/adv.py
@@ -9,3 +9,23 @@ a_list.extend(['d', 'e', 'f'])
 a_list = ['aa', 'bb', 'cc', 'dd', 'aa']
 
 print("Count: {}".format(a_list.count('aa')))
+
+a_set = {1, 2}
+a_set.add(4)
+
+print(a_set)
+
+a_set.update({2, 4, 6, 10, 12, 20})
+print(a_set)
+
+a_set.discard(10)
+
+b_set = a_set.copy()
+
+b_set.add(7)
+b_set.discard(6)
+
+print(a_set)
+print(b_set)
+
+print(b_set.intersection(a_set))
