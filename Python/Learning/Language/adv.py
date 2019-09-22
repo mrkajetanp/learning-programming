4a8e85282644ac4ca711e6d9dd5d780ada913049
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-09-22 20:42:44 +0100

    [Python] Advanced - Generators

--- Python/Learning/Language/adv.py
+++ Python/Learning/Language/adv.py
@@ -50,3 +50,15 @@ print(re.search(pattern, 'M'))
 
 phonePattern = re.compile(r'^(\d{3})-(\d{3})-(\d{4})-(\d+)$')
 print(phonePattern.search('800-555-1212-1234').groups())
+
+def rules(rules_filename):
+    with open(rules_filename, encoding='utf-8') as pattern_file:
+        for line in pattern_file:
+            pattern, search, replace = line.split(None, 2)
+            yield build_match_and_apply_functions(pattern, search, replace)
+
+def plural(noun, rules_filename='plural5-rules.txt'):
+    for matches_rule, apply_rule in rules(rules_filename):
+        if matches_rule(noun):
+            return apply_rule(noun)
+    raise ValueError('no matching rule for {0}'.format(noun))
