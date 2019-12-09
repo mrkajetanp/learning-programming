a61be0dc82d925c076afbfeef9e6c63ccc85bbb8
Cajetan Puchalski <cajetan.puchalski@gmail.com>
2019-12-09 17:50:59 +0000

    [Revision] CS1P Mock Exam #1

--- Python/UofG/Year1/week10.py
+++ Python/UofG/Year1/week10.py
@@ -26,3 +26,34 @@
 # [Revision] CS1F - IM Theoretical Material
 
 # [Revision] CS1F - IM Practical Material
+
+
+print([1, 2, 3][0:2])
+
+def certify(s, certification="CERTIFIED"):
+    return s + " " + certification
+
+def combine(s):
+    return "-".join(s)
+
+def package(elts, pre="(", post=")"):
+    return combine([pre+c+post for c in elts])
+
+values = ["one", "two", "three"]
+print(certify(package(values, "[", "]")))
+
+def flatten(l):
+    stack = list(l)
+    result = []
+    while len(stack) > 0:
+        first = stack.pop(0)
+        print("Both", first, stack)
+        if type(first) == type([]):
+            stack = first + stack
+            print("Stack", stack)
+        else:
+            result.append(first)
+            print("Result", result)
+    return result
+
+print(flatten([[["a", "b"], "c", [["d"]]]]))
