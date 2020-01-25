356a9be75c1395e95759f4bc4e26bffb0581fad2
Kajetan Puchalski <cajetan.puchalski@gmail.com>
2020-01-25 15:50:56 +0000

    [Lecture] CS1P - Easy Bubble Sort implementation

--- Python/UofG/Year1/week13.py
+++ Python/UofG/Year1/week13.py
@@ -32,3 +32,14 @@ print(occurrences([1, 2, 1, 1, 3, 4, 3, 2]))
 
 # [Lecture] CS1P - Time complexity
 
+def bubble_sort(l):
+    result = l
+    for i in range(len(result)):
+        for j in range(i, len(result)):
+            if result[i] > result[j]:
+                result[i], result[j] = result[j], result[i]
+    return l
+
+
+print(bubble_sort([1, 2, 7, 5, 8, 4, 3, 2]))
+
