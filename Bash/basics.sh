bbb1bd8f9cb6625faeefa5cc3be7b0d46c4645c2
Kajetan Puchalski <cajetan.puchalski@gmail.com>
2020-05-10 22:40:25 +0100

    [Bash] Basics

--- Bash/basics.sh
+++ Bash/basics.sh
@@ -121,8 +121,98 @@ fi
 
 ### Arrays
 
-Fruits=('Apple', 'Banana', 'Oranges')
+Fruits=('Apple' 'Banana' 'Oranges')
 echo ${Fruits[1]}
+
+function giveMeArgs() {
+    echo $#
+    echo $*
+    echo $@
+}
+
+echo $(giveMeArgs "ay" "nay" "oke")
+
+echo ${Fruits[@]}
+
+Fruits+=('Watermelon')
+echo ${Fruits[@]}
+
+Fruits=("${Fruits[@]}" "${Veggies[@]}")
+echo ${Fruits[@]}
+
+### Dictionaries
+
+declare -A sounds
+
+sounds[dog]="bark"
+sounds[cow]="moo"
+sounds[bird]="tweet"
+
+echo ${sounds[@]}
+echo ${#sounds[@]}
+
+for val in "${sounds[@]}"; do
+  echo $val
+done
+
+
+echo "something"
+
+a=6
+echo $((a + 200)) 
+echo $((RANDOM%=200))
+
+function caseStatement() {
+    case "$1" in
+        start | up)
+            echo "starting or upping"
+            ;;
+
+        *)
+            echo "Usage: $0 {start|stop|ssh}"
+            ;;
+    esac   
+}
+
+echo $(caseStatement stop)
+
+printf "Hello %s, I'm %s\n" Sven Olga
+printf "1 + 1 = %d\n" 2
+printf "This is how you print a float: %f\n" 2
+
+
+## Getting options
+
+
+function gettingOptions() {
+ while [[ "$1" =~ ^- && ! "$1" == "--" ]]; do case $1 in
+  -V | --version )
+    echo "version"
+    exit
+    ;;
+  -s | --string )
+    shift; string=$1
+    ;;
+  -f | --flag )
+    flag=1
+    echo "flagg"
+    ;;
+esac; shift; done
+if [[ "$1" == '--' ]]; then shift; fi
+}
+
+echo $(gettingOptions --flag)
+
+if ping -c 1 google.com; then
+  echo "It appears you have a working internet connection"
+fi
+
+if grep -q 'foo' ~/.bash_history; then
+  echo "You appear to have typed 'foo' in the past"
+fi
+
+
+
 
 
 
