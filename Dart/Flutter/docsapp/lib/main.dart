537049340213e6d70293ab7abcd23b64e0251649
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-07-15 14:11:35 +0200

    [Flutter] Docs - Layouts - Tutorial

--- Dart/Flutter/docsapp/lib/main.dart
+++ Dart/Flutter/docsapp/lib/main.dart
@@ -1,16 +1,8 @@
 import 'package:flutter/material.dart';
 
 import 'basics.dart';
+import 'layouts.dart';
 
 void main() {
-  runApp(MaterialApp(
-      title: 'Shopping App',
-      home: ShoppingList(
-        products: <Product>[
-          Product(name: 'Eggs'),
-          Product(name: 'Flour'),
-          Product(name: 'Chips'),
-          Product(name: 'Booze'),
-        ],
-      )));
+  runApp(MaterialApp(title: 'Layout Tutorial', home: LayoutTutorial()));
 }
