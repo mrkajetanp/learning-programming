b48b2ec333483e57519c3c4bcc1d76369a572e79
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-07-16 22:48:56 +0200

    [Flutter] Docs - Constraints & Interactivity

--- Dart/Flutter/docsapp/lib/layouts.dart
+++ Dart/Flutter/docsapp/lib/layouts.dart
@@ -22,11 +22,7 @@ class LayoutTutorial extends StatelessWidget {
                   style: TextStyle(color: Colors.grey[500]))
             ],
           )),
-          Icon(
-            Icons.star,
-            color: Colors.red[500],
-          ),
-          Text('41')
+          FavouriteWidget(),
         ],
       ),
     );
@@ -91,6 +87,50 @@ class LayoutTutorial extends StatelessWidget {
                 color: color,
               )),
         ),
+      ],
+    );
+  }
+}
+
+class FavouriteWidget extends StatefulWidget {
+  @override
+  _FavouriteWidgetState createState() => _FavouriteWidgetState();
+}
+
+class _FavouriteWidgetState extends State<FavouriteWidget> {
+  bool _isFavourited = true;
+  int _favouriteCount = 41;
+
+  void _toggleFavourite() {
+    setState(() {
+      if (_isFavourited) {
+        _favouriteCount -= 1;
+        _isFavourited = false;
+      } else {
+        _favouriteCount += 1;
+        _isFavourited = true;
+      }
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+            icon: (_isFavourited ? Icon(Icons.star) : Icon(Icons.star_border)),
+            color: Colors.red[500],
+            onPressed: _toggleFavourite,
+          ),
+        ),
+        SizedBox(
+            width: 18,
+            child: Container(
+              child: Text('$_favouriteCount'),
+            ))
       ],
     );
   }
