6144be2b3370ebd819e741f901377e2fcfa60804
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2020-08-10 22:43:51 +0200

    Ruby on Rails - Getting started

---
+++ C#/.NET/sample1/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace sample1
+{
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Test me");
+        }
+    }
+}
