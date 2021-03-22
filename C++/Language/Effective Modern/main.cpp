85aea40a61fb824a2b4e142331d9ac7971fef263
Kajetan Puchalski <kajetan.puchalski@tuta.io>
2021-03-22 19:33:56 +0000

    Effective Modern C++ - CMake setup

---
+++ C++/Language/Effective Modern/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello there!\n";
+
+    int x = 0;
+
+    if (x < 1) {
+        std::cout << "oopsie" << std::endl;
+    }
+
+    return 0;
+}
